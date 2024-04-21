42ec23e3d2351011ad6bd33ce22e8dd47671022a
saty4a <satyabratbiswal5413@gmail.com>
2024-04-21 23:00:40 +0530

    added bank balance and fixed expenses ui

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="w-full bg-secondary relative bottom-[0px] py-4 rounded-t-xl hover:bg-gray-700">
+    <div className="w-full text-white bg-secondary relative bottom-[0px] py-4 rounded-t-xl hover:bg-gray-700">
       <p className="text-center">
         Made with ❤️ by trinity, @copyrights reserved
       </p>
